7724605553599251f950d514ac729084334b326f
gidgud <jonathanbirkstrom@gmail.com>
2025-10-27 10:27:55 +0100

    GetMapping functions in HomeController class

--- src/main/java/com/elitegroup/uiproject/controller/HomeController.java
+++ src/main/java/com/elitegroup/uiproject/controller/HomeController.java
@@ -1,4 +1,23 @@
 package com.elitegroup.uiproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home/index";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "home/create";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "home/login";
+    }
 }
